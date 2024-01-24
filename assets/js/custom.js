60f39aa6a232301554668e26230b8010b167d1fc
lorensansol <lorensansol@gmail.com>
2024-01-24 22:59:50 +0100

    Update: 2024-01-24 22:59:50

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,11 @@
-const buttonEncuesta = document.querySelector('[href="#encuesta"]')
-if (buttonEncuesta) {
-  const contactFormItemParent = buttonEncuesta.closest('.contact__form-item')
-  function showEncuesta() {
-    contactFormItemParent.classList.toggle('encuesta-activa')
+window.addEventListener('load', () => {
+  const buttonEncuesta = document.querySelector('[href="#encuesta"]')
+  if (buttonEncuesta) {
+    const contactFormItemParent = buttonEncuesta.closest('.contact__form-item')
+    function showEncuesta () {
+      contactFormItemParent.classList.toggle('encuesta-activa')
+    }
+    showEncuesta()
+    buttonEncuesta.addEventListener('click', showEncuesta)
   }
-  showEncuesta()
-  buttonEncuesta.addEventListener('click', showEncuesta)
-}
+})
